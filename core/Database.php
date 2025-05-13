c5b2494421acf40459f6465481be7aa011667298
Benjamin Ebo Yankson <ben.macfinn@gmail.com>
2025-05-13 03:49:36 +0000

    Update default database name

--- core/Database.php
+++ core/Database.php
@@ -8,7 +8,7 @@ class Database
     {
         // You can load these from a config file or environment
         $host = getenv('DB_HOST') ?: 'localhost';
-        $dbname = getenv('DB_NAME') ?: 'test';
+        $dbname = getenv('DB_NAME') ?: 'alivechms';
         $user = getenv('DB_USER') ?: 'root';
         $pass = getenv('DB_PASS') ?: '';
         $charset = 'utf8mb4';
