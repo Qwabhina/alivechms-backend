5bf6d05bea516b1ce1bc639b0037f745ce1ba15f
Benjamin Ebo Yankson <ben.macfinn@gmail.com>
2025-05-14 13:31:36 +0000

    Add headers

--- index.php
+++ index.php
@@ -13,7 +13,9 @@ $dotenv->load();
 require_once __DIR__ . '/core/ORM.php';
 require_once __DIR__ . '/core/Auth.php';
 
-
+header('Access-Control-Allow-Origin: *'); // Adjust to specific origin in production
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 header('Content-Type: application/json');
 
