b1bcba11c8ff543df08b2c4b4cdc2848ca4eeb2a
Benjamin Ebo Yankson <ben.macfinn@gmail.com>
2025-05-15 17:46:59 +0000

    Remove headers

--- index.php
+++ index.php
@@ -13,10 +13,6 @@ $dotenv->load();
 require_once __DIR__ . '/core/ORM.php';
 require_once __DIR__ . '/core/Auth.php';
 
-header('Access-Control-Allow-Origin: *'); // Adjust to specific origin in production
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-
 header('Content-Type: application/json');
 
 
